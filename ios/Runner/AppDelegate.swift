87722a4c99ddb56156823fdc156ea15321983b85
Toby <tobyau9000@gmail.com>
2020-03-06 09:37:37 -0500

    added analytics, firestore, auth

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,30 +2,31 @@ import UIKit
 import Flutter
 import Firebase
 
-//@UIApplicationMain
-//@objc class AppDelegate: FlutterAppDelegate {
-//
-//  override func application(
-//    _ application: UIApplication,
-//    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-//  ) -> Bool {
-//    GeneratedPluginRegistrant.register(with: self)
-//    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-//  }
-//
-//}
-
-
-
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+@objc class AppDelegate: FlutterAppDelegate {
 
-  var window: UIWindow?
+ override func application(
+   _ application: UIApplication,
+   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+ ) -> Bool {
+   FirebaseApp.configure()
+   GeneratedPluginRegistrant.register(with: self)
+   return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+ }
 
-  func application(_ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions:
-    [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    FirebaseApp.configure()
-    return true
-  }
 }
+
+
+
+// @UIApplicationMain
+// class AppDelegate: UIResponder, UIApplicationDelegate {
+
+//   var window: UIWindow?
+
+//   func application(_ application: UIApplication,
+//     didFinishLaunchingWithOptions launchOptions:
+//     [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+//     FirebaseApp.configure()
+//     return true
+//   }
+// }
