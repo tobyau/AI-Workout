dcef567b69c7fdc80896ac262956de809150dfa2
Shanjida6 <shanjida.a@gmail.com>
2020-04-12 21:05:47 -0400

    add results page and modified pubspec.yaml

--- lib/pages/ResultsPage.dart
+++ lib/pages/ResultsPage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
+const PrimaryColor = const Color(0xFFFFFF);
 class ResultsPage extends StatefulWidget {
   
   @override 
@@ -7,30 +9,64 @@ class ResultsPage extends StatefulWidget {
       return _ResultsPageState();
     }
 }
-
 class _ResultsPageState extends State<ResultsPage> {
+  VideoPlayerController _controller;
+  bool _isPlaying = false;
   
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(
+        'http://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4')
+        ..addListener(() {
+          final bool isPlaying = _controller.value.isPlaying;
+          if(isPlaying != _isPlaying){
+            setState((){
+              _isPlaying = isPlaying;
+            });
+          }
+        })
+      ..initialize().then((_){
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {
+        });
+      });
+  }
 
   @override 
-  Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: <Widget>[
-        SliverAppBar(
-          //backgroundColor: Colors.white,
-          expandedHeight: 150.0,
-          floating: true,
-          pinned: true, 
-          flexibleSpace: FlexibleSpaceBar(
-            title: Text(
-              'Results', 
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 15
-              ) 
-            ),
+  Widget build(BuildContext context){
+    //* 
+    return MaterialApp(
+      title: 'Video Demo',
+      home: Scaffold(
+        body: Center(
+          child: _controller.value.initialized
+              ? AspectRatio(
+                  aspectRatio: _controller.value.aspectRatio,
+                  child: VideoPlayer(_controller),
+                )
+              : Container(),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              _controller.value.isPlaying
+                  ? _controller.pause()
+                  : _controller.play();
+            });
+          },
+          child: Icon(
+            _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
           ),
         ),
-      ]
-    );
+
+        appBar:AppBar(
+          title: Image.asset('assets/stats.png', fit: BoxFit.cover),
+          backgroundColor: PrimaryColor,
+        ), 
+        //body: Text("Hello Flutter"),
+        ),
+      );
   }
 }
+
