a0a19360862f0a534f14a691e26a2059d87219aa
R-Ligier <rebeccal784@gmail.com>
2020-05-03 21:04:58 -0400

    need to finish splash page

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -20,7 +20,7 @@ class _SplashScreenState extends State<SplashScreen> {
   }
   
   Future<Timer> loadData() async {
-    return new Timer(Duration(seconds: 2), onDoneLoading);
+    return new Timer(Duration(seconds: 10), onDoneLoading);
   }
   
   onDoneLoading() async {
@@ -38,7 +38,7 @@ class _SplashScreenState extends State<SplashScreen> {
       decoration: BoxDecoration(
         image: DecorationImage(
           image: AssetImage('assets/splash1.jpg'),
-          fit: BoxFit.fitWidth,
+          fit: BoxFit.contain,
           alignment: Alignment.bottomLeft,
         ),
       ),
